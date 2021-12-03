c3675ab9e5bc3b3bf6a7f958df6be79dc228c175
LUCIO Notebook <luciononato@hotmail.com>
2021-12-03 20:04:41 -0300

    Controller

--- src/main/java/com/example/atsd201751087727/controller/EnderecoController.java
+++ src/main/java/com/example/atsd201751087727/controller/EnderecoController.java
@@ -1,11 +1,52 @@
 package com.example.atsd201751087727.controller;
 
+import com.example.atsd201751087727.entity.Endereco;
 import com.example.atsd201751087727.service.EnderecoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class EnderecoController {
     @Autowired
     private EnderecoService service;
+
+    @PostMapping
+    public Endereco addEndereco(@RequestBody Endereco endereco){
+        return service.saveEndereco(endereco);
+    }
+
+    @PostMapping
+    public List<Endereco> addEndereco(@RequestBody List<Endereco> enderecos){
+        return service.saveEnderecos(enderecos);
+    }
+
+    @GetMapping
+    public List<Endereco> findAllEndereco(){
+        return service.getEnderecos();
+    }
+
+    @GetMapping
+    public Endereco findEnderecoById(@PathVariable int id) {
+        return service.getEnderecoById(id);
+    }
+
+    @GetMapping
+    public Endereco findEnderecoByName(@PathVariable String name) {
+        return service.getEnderecoByLogradouro(name);
+    }
+
+    @PutMapping
+    public Endereco updateEndereco(@RequestBody Endereco endereco)
+    {
+        return service.updateEndereco(endereco);
+    }
+
+    @DeleteMapping
+    public String deleteEndereco(@PathVariable int id) {
+        return service.deleteEndereco(id);
+    }
+
 }
--- src/main/java/com/example/atsd201751087727/controller/PessoaController.java
+++ src/main/java/com/example/atsd201751087727/controller/PessoaController.java
@@ -1,15 +1,49 @@
 package com.example.atsd201751087727.controller;
 
+import com.example.atsd201751087727.entity.Pessoa;
 import com.example.atsd201751087727.service.PessoaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class PessoaController {
     @Autowired
     private PessoaService service;
 
+    @PostMapping
+    public Pessoa addPessoa(@RequestBody Pessoa pessoa){
+        return service.savePessoa(pessoa);
+    }
 
+    @PostMapping
+    public List<Pessoa> addPessoas(@RequestBody List<Pessoa> pessoas){
+        return service.savePessoas(pessoas);
+    }
 
+    @GetMapping
+    public List<Pessoa> findAllPessoas(){
+        return service.getPessoas();
+    }
+
+    @GetMapping
+    public Pessoa findPessoaById(@PathVariable int id) {
+        return service.getPessoaById(id);
+    }
+
+    @GetMapping
+    public Pessoa findPessoaByName(@PathVariable String name) {
+        return service.getPessoaByName(name);
+    }
+
+    @PutMapping
+    public Pessoa updatePessoa(@RequestBody Pessoa pessoa) {
+        return service.updatePessoa(pessoa);
+    }
+
+    @DeleteMapping
+    public String deletePessoa(@PathVariable int id) {
+        return service.deletePessoa(id);
+    }
 }
